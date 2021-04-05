a7bd3004441098390b473c57c4ea735381291522
RedaMeziouni <meziounidb@gmail.com>
2021-04-05 12:58:24 +0100

    Make login form

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 <html>
 <head>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="login.css">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<title>CRUD | Login</title>
@@ -17,12 +18,12 @@
 
     <!-- Icon -->
     <div class="fadeIn first">
-      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+      <img src="logo.png" id="icon" alt="User Icon" />
     </div>
 
     <!-- Login Form -->
     <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Username">
       <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
