da02530efed06f77b4c0225a2c82505d27e9bc7f
RedaMeziouni <meziounidb@gmail.com>
2021-04-06 16:18:17 +0100

    Create Choise page

---
+++ Customer/choise.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Choise</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body> 
+ 
+<div class="container">
+  <h2>Cards Columns</h2>
+  <br>
+  <div class="card-columns">
+    <div class="card bg-primary">
+      <div class="card-body text-center">
+        <a style="cursor: pointer;">Animals</a>
+    </div>
+    </div>
+    <div class="card bg-warning">
+      <div class="card-body text-center">
+      <a style="cursor: pointer;">Birds</a>
+    </div>
+    </div>
+</div>
+
+</body>
+</html>
