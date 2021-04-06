470e546b5e2275fef16c250ccb7d09d781c2adb5
RedaMeziouni <meziounidb@gmail.com>
2021-04-06 12:55:26 +0100

    login done

--- login.php
+++ login.php
@@ -1,9 +1,37 @@
 <?php  
 // After you get connected you should be redirected to the home page
-?>
+  include_once('includes/db-inc.php'); 
+  if(isset($_POST['submit'])){
+
+    $uname = mysqli_real_escape_string($con, $_POST['username']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+
+    if ($uname != "pets" && $password != "123456"){
+
+        $sql_query = "SELECT count(*) as cntUser from users where username='".$uname."' and password='".$password."'";
+        // echo "<pre>";
+        // echo $uname;
+        // echo "</pre>";
+        // $sql_query = "SELECT * FROM users WHERE username='".$username."' AND password='".$password."'";
+        // $sql_query = "SELECT * FROM user WHERE username='pets' AND password='123456'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['uname'] = $uname;
+            header('Location: ./Customer/dashboard.php');
+        }else{
+            echo "Invalid username or password";
+        }
+
+    }
+  }
+    ?>
 
 <!DOCTYPE html>
-<html>
+<html class="fix1">
 <head>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" href="./CSS/main.css">
@@ -12,25 +40,22 @@
 	<title>CRUD | Login</title>
 </head>
 <body>
-
-
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
-
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="./assets/logo.png" id="icon" alt="User Icon" />
-    </div>
-
-    <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Username">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+<div class="container">
+    <form method="post" action="./Customer/dashboard.php">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" name="username" placeholder="Username" required autocomplete="off">
+            </div>
+            <div>
+                <input type="password" class="textbox" name="password" placeholder="Password" required autocomplete="off">
+            </div>
+            <div>
+                <input type="submit" value="Submit" name="submit">
+            </div>
+        </div>
     </form>
-
-  </div>
 </div>
 </body>
+
 </html>
